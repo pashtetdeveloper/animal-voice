f759c0c24c8e7b536ca1b93c6f2997fc03cd99f9
Pavlo Salnikov <105820801+pashtetdeveloper@users.noreply.github.com>
2025-09-19 14:38:41 +0200

    Fix React Native image loading with require()

---
+++ data/animals.ts
@@ -0,0 +1,322 @@
+import { Animal } from '../types/Animal';
+
+export const allAnimals: Animal[] = [
+  {
+    id: 1,
+    name: 'Cat',
+    image: require('../assets/images/animals/cat.png'),
+    sound: '🐱 Meow!'
+  },
+  {
+    id: 2,
+    name: 'Cow',
+    image: require('../assets/images/animals/cow.png'),
+    sound: '🐄 Moo!'
+  },
+  {
+    id: 3,
+    name: 'Dog',
+    image: require('../assets/images/animals/dog.png'),
+    sound: '🐶 Woof!'
+  },
+  {
+    id: 4,
+    name: 'Owl',
+    image: require('../assets/images/animals/owl.png'),
+    sound: '🦉 Hoot!'
+  },
+  {
+    id: 5,
+    name: 'Pig',
+    image: require('../assets/images/animals/pig.png'),
+    sound: '🐷 Oink!'
+  },
+  {
+    id: 6,
+    name: 'Bear',
+    image: require('../assets/images/animals/bear.png'),
+    sound: '🐻 Roar!'
+  },
+  {
+    id: 7,
+    name: 'Boar',
+    image: require('../assets/images/animals/boar.png'),
+    sound: '🐗 Grunt!'
+  },
+  {
+    id: 8,
+    name: 'Deer',
+    image: require('../assets/images/animals/deer.png'),
+    sound: '🦌 Snort!'
+  },
+  {
+    id: 9,
+    name: 'Duck',
+    image: require('../assets/images/animals/duck.png'),
+    sound: '🦆 Quack!'
+  },
+  {
+    id: 10,
+    name: 'Frog',
+    image: require('../assets/images/animals/frog.png'),
+    sound: '🐸 Ribbit!'
+  },
+  {
+    id: 11,
+    name: 'Goat',
+    image: require('../assets/images/animals/goat.png'),
+    sound: '🐐 Bleat!'
+  },
+  {
+    id: 12,
+    name: 'Gull',
+    image: require('../assets/images/animals/gull.png'),
+    sound: '🕊️ Squawk!'
+  },
+  {
+    id: 13,
+    name: 'Lion',
+    image: require('../assets/images/animals/lion.png'),
+    sound: '🦁 Roar!'
+  },
+  {
+    id: 14,
+    name: 'Puma',
+    image: require('../assets/images/animals/puma.png'),
+    sound: '🐆 Growl!'
+  },
+  {
+    id: 15,
+    name: 'Seal',
+    image: require('../assets/images/animals/seal.png'),
+    sound: '🦭 Bark!'
+  },
+  {
+    id: 16,
+    name: 'Wolf',
+    image: require('../assets/images/animals/wolf.png'),
+    sound: '🐺 Howl!'
+  },
+  {
+    id: 17,
+    name: 'Camel',
+    image: require('../assets/images/animals/camel.png'),
+    sound: '🐪 Grunt!'
+  },
+  {
+    id: 18,
+    name: 'Eagle',
+    image: require('../assets/images/animals/eagle.png'),
+    sound: '🦅 Screech!'
+  },
+  {
+    id: 19,
+    name: 'Goose',
+    image: require('../assets/images/animals/goose.png'),
+    sound: '🪿 Honk!'
+  },
+  {
+    id: 20,
+    name: 'Horse',
+    image: require('../assets/images/animals/horse.png'),
+    sound: '🐴 Neigh!'
+  },
+  {
+    id: 21,
+    name: 'Hyena',
+    image: require('../assets/images/animals/hyena.png'),
+    sound: '🐺 Laugh!'
+  },
+  {
+    id: 22,
+    name: 'Mouse',
+    image: require('../assets/images/animals/mouse.png'),
+    sound: '🐭 Squeak!'
+  },
+  {
+    id: 23,
+    name: 'Raven',
+    image: require('../assets/images/animals/raven.png'),
+    sound: '🐦‍⬛ Caw!'
+  },
+  {
+    id: 24,
+    name: 'Sheep',
+    image: require('../assets/images/animals/sheep.png'),
+    sound: '🐑 Baa!'
+  },
+  {
+    id: 25,
+    name: 'Tiger',
+    image: require('../assets/images/animals/tiger.png'),
+    sound: '🐅 Roar!'
+  },
+  {
+    id: 26,
+    name: 'Zebra',
+    image: require('../assets/images/animals/zebra.png'),
+    sound: '🦓 Neigh!'
+  },
+  {
+    id: 27,
+    name: 'Baboon',
+    image: require('../assets/images/animals/baboon.png'),
+    sound: '🐒 Chatter!'
+  },
+  {
+    id: 28,
+    name: 'Beluga',
+    image: require('../assets/images/animals/beluga.png'),
+    sound: '🐋 Whistle!'
+  },
+  {
+    id: 29,
+    name: 'Coyote',
+    image: require('../assets/images/animals/coyote.png'),
+    sound: '🐺 Yip!'
+  },
+  {
+    id: 30,
+    name: 'Donkey',
+    image: require('../assets/images/animals/donkey.png'),
+    sound: '🫏 Bray!'
+  },
+  {
+    id: 31,
+    name: 'Falcon',
+    image: require('../assets/images/animals/falcon.png'),
+    sound: '🦅 Screech!'
+  },
+  {
+    id: 32,
+    name: 'Guenon',
+    image: require('../assets/images/animals/guenon.png'),
+    sound: '🐒 Chatter!'
+  },
+  {
+    id: 33,
+    name: 'Jackal',
+    image: require('../assets/images/animals/jackal.png'),
+    sound: '🐺 Howl!'
+  },
+  {
+    id: 34,
+    name: 'Parrot',
+    image: require('../assets/images/animals/parrot.png'),
+    sound: '🦜 Squawk!'
+  },
+  {
+    id: 35,
+    name: 'Racoon',
+    image: require('../assets/images/animals/racoon.png'),
+    sound: '🦝 Chitter!'
+  },
+  {
+    id: 36,
+    name: 'Buffalo',
+    image: require('../assets/images/animals/buffalo.png'),
+    sound: '🐃 Snort!'
+  },
+  {
+    id: 37,
+    name: 'Chicken',
+    image: require('../assets/images/animals/chicken.png'),
+    sound: '🐓 Cluck!'
+  },
+  {
+    id: 38,
+    name: 'Dolphin',
+    image: require('../assets/images/animals/dolphin.png'),
+    sound: '🐬 Click!'
+  },
+  {
+    id: 39,
+    name: 'Gorilla',
+    image: require('../assets/images/animals/gorilla.png'),
+    sound: '🦍 Grunt!'
+  },
+  {
+    id: 40,
+    name: 'Leopard',
+    image: require('../assets/images/animals/leopard.png'),
+    sound: '🐆 Growl!'
+  },
+  {
+    id: 41,
+    name: 'Penguin',
+    image: require('../assets/images/animals/penguin.png'),
+    sound: '🐧 Trumpet!'
+  },
+  {
+    id: 42,
+    name: 'Roebuck',
+    image: require('../assets/images/animals/roebuck.png'),
+    sound: '🦌 Bark!'
+  },
+  {
+    id: 43,
+    name: 'Anteater',
+    image: require('../assets/images/animals/anteater.png'),
+    sound: '🐜 Snort!'
+  },
+  {
+    id: 44,
+    name: 'Elephant',
+    image: require('../assets/images/animals/elephant.png'),
+    sound: '🐘 Trumpet!'
+  },
+  {
+    id: 45,
+    name: 'Flamingo',
+    image: require('../assets/images/animals/flamingo.png'),
+    sound: '🦩 Honk!'
+  },
+  {
+    id: 46,
+    name: 'Squirrel',
+    image: require('../assets/images/animals/squirrel.png'),
+    sound: '🐿️ Chatter!'
+  },
+  {
+    id: 47,
+    name: 'Alligator',
+    image: require('../assets/images/animals/alligator.png'),
+    sound: '🐊 Hiss!'
+  },
+  {
+    id: 48,
+    name: 'Chimpanzee',
+    image: require('../assets/images/animals/chimpanzee.png'),
+    sound: '🐵 Screech!'
+  },
+  {
+    id: 49,
+    name: 'Rhinoceros',
+    image: require('../assets/images/animals/rhinoceros.png'),
+    sound: '🦏 Snort!'
+  },
+  {
+    id: 50,
+    name: 'Grasshopper',
+    image: require('../assets/images/animals/grasshopper.png'),
+    sound: '🦗 Chirp!'
+  },
+  {
+    id: 51,
+    name: 'Hippopotamus',
+    image: require('../assets/images/animals/hippopotamus.png'),
+    sound: '🦛 Grunt!'
+  },
+  {
+    id: 52,
+    name: 'Killer Whale',
+    image: require('../assets/images/animals/killer-whale.png'),
+    sound: '🐋 Click!'
+  },
+  {
+    id: 53,
+    name: 'Black Panther',
+    image: require('../assets/images/animals/black-panther.png'),
+    sound: '🐆 Growl!'
+  }
+];
---
+++ types/Animal.ts
@@ -0,0 +1,8 @@
+@@ .. @@
+ export interface Animal {
+   id: number;
+   name: string;
+-  image: string;
++  image: any;
+   sound: string;
+ }
